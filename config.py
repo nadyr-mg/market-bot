83dcfa03c1f081bea7a1086b71ec2580c41fe40b
Said <said.salem@web.de>
2018-06-10 10:13:13 +0300

    added Lykke pairs to all 4 config files
    (cherry picked from commit fb78f27)

--- config.py
+++ config.py
@@ -5,20 +5,12 @@ from os.path import join
 
 from configuration_files import config_dir
 
-API_KEY = "1c15115b-b5b6-4920-9629-4c444e346613"
 
-PAIRS = ['WAX/ETH']
-# PAIRS = ['LOC/ETH', 'WAX/ETH', 'CVC/ETH']
-COIN_IDS = {
-    "ETH": "ETH",
-    "WAX": "6e25e8ab-5779-4543-855b-71f4857b47d5",
-    # "LOC": "572475a4-8fef-4e39-909e-85f6bbbc10c4",
-    # "WTC": "168f13bf-bfea-4931-91ff-e449850d694e",
-    # "PPT": "98385941-89b3-45c2-ae8e-b64c6f3bbac9",
-    # "CVC": "f9fb5970-2fc4-4b08-900b-870f245e430b",
-}
-MIN_SPREAD = 10
-PERIOD = 15
+API_KEY = "1c15115b-b5b6-4920-9629-4c444e346613"
+PAIRS = ['LYKKE/USD']
+# PAIRS = ['LOC/ETH', 'WAX/ETH', 'CVC/ETH','LYKKE/USD']
+MIN_SPREAD = 3  ### Spread chhanged for testing purose
+PERIOD = 30
 BALANCE_REMAIN_PART = 0.7
 
 ACCEPTABLE_PROFIT_DEVIATION = 0.05
@@ -32,6 +24,24 @@ REF_BOOK_RELEVANCE_TIME = 5 * MINUTE
 
 AFTER_CANCEL_WAIT_BOUNDS = (3 * MINUTE, 7 * MINUTE)
 
+COIN_IDS = {
+    "ETH": "ETH",
+    "WAX": "6e25e8ab-5779-4543-855b-71f4857b47d5",
+    "LOC": "572475a4-8fef-4e39-909e-85f6bbbc10c4",
+    # "WTC": "168f13bf-bfea-4931-91ff-e449850d694e",
+    # "PPT": "98385941-89b3-45c2-ae8e-b64c6f3bbac9",
+    "CVC": "f9fb5970-2fc4-4b08-900b-870f245e430b",
+    #    "AE": "6f75280b-a005-4016-a3d8-03dc644e8912",
+    "LYKKE": "LKK",
+    "CHF": "CHF",
+    "EUR": "EUR",
+    "USD": "USD",
+    #    "BTC": "BTC",
+    #    "GBP": "GBP",
+    #    "JPY": "JPY",
+}
+
+
 with open(join(config_dir, "reference_markets.json")) as file:
     REF_MARKETS = load(file)  # type: Dict
 
@@ -43,27 +53,5 @@ with open(join(config_dir, "min_amounts.json")) as file:
 with open(join(config_dir, "ref_deviations.json")) as file:
     REF_PRICE_DEVIATIONS = load(file)  # type: Dict
 
-# how many lines of logs to send by an email if unexpected error occurs
-LINES_TO_SEND = 15
-
-FROM_EMAIL = 'Your gmail address'
-LOGIN, PASSW = FROM_EMAIL, "passw"  # credentials for authentication in your gmail account
-TO_EMAIL = 'Address where messages should come'
-
-LOG_FILENAME = 'info.log'
-
 # setup default logging level
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-# create console handler and set level to info
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-# create error file handler and set level to error
-handler = logging.FileHandler(LOG_FILENAME, 'w')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
